1592cd01c71707424ed0fa919bd8aa9a1db2ea64
odysseaspapadimas <odysseas.patra@gmail.com>
2021-07-26 17:55:03 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const http = require("http");
+const express = require("express");
+const socketio = require("socket.io");
+const cors = require("cors");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+const server = http.createServer(app);
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+app.get("/", (req, res) => {
+  res.send({ response: "Server is up and running" });
+});
+
+io.on("connection", (socket) => {
+  socket.on("emergency", ({ senderId, date }) => {
+    console.log("sender", senderId);
+    io.emit("emergency", { senderId, date });
+  });
+  socket.on("disconnect", () => {});
+});
+
+server.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
